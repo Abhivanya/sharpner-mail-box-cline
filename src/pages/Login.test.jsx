e909de30d53de32882f555603d325552bbf7b814
Abhivanya <your_email@example.com>
2024-12-16 12:47:03 +0530

    working on mail

--- src/pages/Login.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Login from "./Login"; // Ensure this path matches your project structure
-import { it } from "vitest";
-
-it("renders the Login component", () => {
-  render(<Login />);
-  expect(screen.getByText("Login")).toBeInTheDocument();
-});
